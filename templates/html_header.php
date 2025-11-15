22299822dc30d595a3cbf14f1c6ba6091162961c
Adam Thomas <thomasadam83@hotmail.com>
2025-11-14 23:47:10 -0500

    Update page title format

--- templates/html_header.php
+++ templates/html_header.php
@@ -4,7 +4,7 @@
 
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?=PAGE_TITLE?> | <?=APP_NAME?> | BrickMMO Console </title>
+        <title><?=PAGE_TITLE?> | <?=APP_NAME?> | BrickMMO</title>
 
         
     <!-- W3 School CSS -->
